5977fba66032845437b75e9576892c1af4b270df
Judge Dredd <the.guard@mail.ru>
2013-09-01 10:19:43 +0400

    security enhacenent using 2 keys

--- srv.c
+++ srv.c
@@ -12,6 +12,7 @@
 #define UDP_PORT 5005
 #define TIME_LIMIT 1 /* seconds */
 
+/* would be simpler in asm */
 union c2i {
   char b[4];
   long int i;
@@ -19,14 +20,15 @@ union c2i {
 
 int main(int argc, char* argv[]){
   struct sockaddr_in si_srv, si_cli;
-  int sock, i = sizeof(struct sockaddr_in), recvd, j, key, conf_msg;
+  int sock, i = sizeof(struct sockaddr_in), recvd, j, key[2], conf_msg;
   char buf[BUFFSZ];
   union c2i d;
   /* need blacklisted ips and seeion ips */
   /* srandom(unsigned int seed); */
 
-  key = random();
-  
+  key[0] = random();
+  key[1] = random();
+
   if( (sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1 )
     perror("SRV\terror openong a socket"), exit(0);
 
@@ -57,9 +59,9 @@ int main(int argc, char* argv[]){
     if( !(buf[0] && buf[1]) && (recvd == 2) ){
       /* this client connecting the first time
 	 send the confirmation request */
-      conf_msg = si_cli.sin_addr.s_addr ^ time(NULL);
+      conf_msg = si_cli.sin_addr.s_addr ^ (time(NULL) + key[0]);
       /* use gettimeofday() for usec instead? */
-      conf_msg ^= key;
+      conf_msg ^= key[1];
       
       sendto(sock, &conf_msg, sizeof(conf_msg), 0, (struct sockaddr *) &si_cli, 
 	     sizeof(struct sockaddr_in));
@@ -69,10 +71,10 @@ int main(int argc, char* argv[]){
       
       for(j = 0; j < sizeof(d); j++)
 	d.b[j] = buf[j];
-      conf_msg = d.i ^ key;
+      conf_msg = d.i ^ key[1];
       conf_msg ^= si_cli.sin_addr.s_addr;
 
-      if( (time(NULL) -  conf_msg) < TIME_LIMIT ){
+      if( (time(NULL) -  (conf_msg - key[0]) ) < TIME_LIMIT ){
 	/* add the client ip to the session list */
 	printf("SRV:\t%s accepted to session list\n",
 	       inet_ntoa(si_cli.sin_addr));
